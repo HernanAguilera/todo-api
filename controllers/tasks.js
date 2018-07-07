47fe4645d993203a5632066477234e0f42d00190
Hernán Aguilera <haguilera@openmailbox.org>
2018-07-07 08:09:58 -0500

    Updated json structure of response

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,7 +4,7 @@ const Task = require('../models/Task')
 
 function list(req, res) {
     Task.find({}, (error, tasks) => {
-        if(error) res.status(500).send(`Ha ocurrido un error al obtener los elementos: ${error}`)
+        if(error) res.status(500).send({message: `Ha ocurrido un error al obtener los elementos: ${error}`})
         res.send(tasks)
     })
 }
@@ -16,7 +16,7 @@ function create(req, res) {
     })
 
     task.save((error, taskSaved) => {
-        if(error) return res.status(500).send(`Ha ocurrido un error al intentar guardar el elemento: ${error}`)
+        if(error) return res.status(500).send({message: `Ha ocurrido un error al intentar guardar el elemento: ${error}`})
 
         res.send(taskSaved)
     })
@@ -26,9 +26,9 @@ function get(req, res) {
     var id = req.params.id
 
     Task.findById(id, (error, task) => {
-        if(error) return res.status(500).send(`Ha ocurrido un error al intentar obtener el elemento: ${error}`)
+        if(error) return res.status(500).send({message: `Ha ocurrido un error al intentar obtener el elemento: ${error}`})
 
-        if(!task) return res.status(404).send('La tarea solicitada no ha pidido ser encontrada')
+        if(!task) return res.status(404).send({message: 'La tarea solicitada no ha pidido ser encontrada'})
 
         res.send(task)
     })
@@ -39,7 +39,7 @@ function update(req, res) {
     var data = req.body
     console.log(data)
     Task.findByIdAndUpdate(id, data, (error, task) => {
-        if(error) return res.status(500).send('Ha ocurrido un error')
+        if(error) return res.status(500).send({message: 'Ha ocurrido un error'})
         
         res.send(Object.assign(task, data))
     })
@@ -51,12 +51,12 @@ function remove(req, res) {
     Task.findById(id, (error, task) => {
         if(error) return res.status(500).send({message: `se ha producido un error al buscar el elmento solicitado: ${error}`})
 
-        if(!task) return res.status(404).send('La tarea solicitada no ha pidido ser encontrada')
+        if(!task) return res.status(404).send({message: 'La tarea solicitada no ha pidido ser encontrada'})
 
         task.remove(error => {
             if(error) return res.status(500).send({message: `se ha producido un error al intentar eliminar el elemento: ${error}`})
 
-            res.send('La tarea ha sido eliminada.')
+            res.send({message: 'La tarea ha sido eliminada.'})
         })
     })
 }
